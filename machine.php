6baf2e17ce3c4c00310d27ca25649c16e8bcd1bf
Konrad Cholewa <KonradCholewa@optimal.local>
2019-07-31 16:00:52 +0200

    function addmoney, update, viewCash, buyItem, returnMoney; adding main class

--- machine.php
+++ machine.php
@@ -1,35 +1,88 @@
 <?php
 class machine{
-    private $nickiel = 0;
-    private $dime = 0;
-    private $quarter = 0;
-    private $dollar = 0;
+    //condition of coins thrown
+    private $nickiel = 20;
+    private $dime = 20;
+    private $quarter = 1;
+    private $dollar =-1;
 
-    public $cash = 0;
+    //budget for the purchase of the item
+    private $cash = 0;
 
+    //item list
+    private $items = ["item0" => 0.65 , "item1" => 1, "item2" => 1.5];
+
+
+    //function add money
     public function addMoney($i){
         switch ($i) {
             case 0:
-                $this->$nickiel;
+                $this->nickiel++;
+                $this->update(0.05);
                 break;
             case 1:
-                $this->$dime++;
+                $this->dime++;
+                $this->update(0.10);
                 break;
             case 2:
-                $this->$quarter++;
+                $this->quarter++;
+                $this->update(0.25);
                 break;
             case 3:
-                $this->$dollar++;
+                $this->dollar++;
+                $this->update(1.00);
         }
-        $this->update();
     }
 
-    private function update(){
-        $this->cash = $this->$nickiel * 0.05 + $this->$dime * 0.10 + $this->$quarter * 0.25 + $this->$dollar * 1;
+    private function update($i){
+        $this->cash = $this->cash + $i;
     }
 
     public function viewCash(){
-        $sad = $this->$cash;
+      echo $this->cash;
     } 
+
+    public function buyItem($i){
+        if($this->cash > $this->items['item'.$i]){
+            echo "You bought the item";
+            $this->cash = $this->cash - $this->items['item'.$i];
+            echo "return money";
+        }else{
+            echo "More money";
+        }
+    
+    }
+
+    public function returnMoney(){
+        $returnCash = $this->cash;
+
+        $dollar = floor($returnCash / 1);
+        if($dollar <= $this->dollar){
+            $returnCash = $returnCash - $dollar;
+            $this->dollar = $this->dollar - $dollar;  
+            echo $dollar . " x dollar";
+        }elseif($this->dollar < $dollar && $this->dollar > 0 ){
+            //how much can you give away coins
+            $cacheCash = $dollar - $this->dollar;
+            $returnCash = $returnCash - $cacheCash;
+            $this->dollar = $this->dollar - $cacheCash;  
+            echo $cacheCash . " x dollar";
+        }
+
+        $quarter = floor($returnCash / 0.25);
+        if($quarter <= $this->quarter){
+            $returnCash = $returnCash - $quarter * 0.25;
+            $this->quarter = $this->quarter - $quarter;
+            echo $quarter . " x quarter";
+        }elseif($this->quarter < $quarter && $this->quarter > 0 ){
+            //how much can you give away coins
+            $cacheCash = $quarter - $this->quarter;
+            $returnCash = $returnCash - $cacheCash*0.25;
+            $this->quarter = $this->quarter - $cacheCash;  
+            echo $cacheCash . " x quarter";
+        }
+        
+        
+    }
 
 }
