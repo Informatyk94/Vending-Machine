03274c0d9755babe0f1f26497ecefbd68045fed0
Informatyk94 <30533623+Informatyk94@users.noreply.github.com>
2019-08-03 16:25:45 +0200

    finished

--- machine.php
+++ machine.php
@@ -1,20 +1,27 @@
 <?php
 class machine{
     //condition of coins thrown
-    private $nickiel = 20;
-    private $dime = 20;
-    private $quarter = 20;
-    private $dollar =20;
+    private $nickiel    = 20;
+    private $dime       = 20;
+    private $quarter    = 20;
+    private $dollar     = 20;
+
     private $typeOfMoneyAndState = [
-        ["id"=> 0, "name" => "dollar" ,     "value" => 1.00,    "quantity" => 20],
-        ["id"=> 1, "name" => "quarter" ,    "value" => 0.25,    "quantity" => 20],
-        ["id"=> 2, "name" => "dime" ,       "value" => 0.10,    "quantity" => 20],
-        ["id"=> 3, "name" => "nickiel" ,    "value" => 0.05,    "quantity" => 20],
+        ["id"=> 0, "name" => "dollar" , "value" => 1,   "quantity" => 20],
+        ["id"=> 1, "name" => "quarter" ,"value" => 0.25,"quantity" => 20],
+        ["id"=> 2, "name" => "dime" ,   "value" => 0.1,"quantity" => 20],
+        ["id"=> 3, "name" => "nickiel" ,"value" => 0.05,"quantity" => 20],
     ];
+
     //budget for the purchase of the item
     private $cash = 0;
+
     //item list
-    private $items = ["item0" => 0.65 , "item1" => 1, "item2" => 1.5];
+    private $items = ["item0" => 0.65,     
+                      "item1" => 1, 
+                      "item2" => 1.5];
+
+
     //function add money
     public function addMoney($i){
         foreach($this->typeOfMoneyAndState as &$type){
@@ -22,68 +29,44 @@ class machine{
                 $type["quantity"] = $type["quantity"] + 1;
                 $this->update($type["value"]);
             }
-        }
+        }    
     }
+
     private function update($i){
         $this->cash = $this->cash + $i;
     }
 
     public function viewCash(){
         echo $this->cash . " ";
-    }
-    
+    } 
+
     public function buyItem($i){
         if($this->cash >= $this->items['item'.$i]){
-            echo "You bought the item";
+            echo "You bought the item <br />";
             $this->cash = $this->cash - $this->items['item'.$i];
-            echo "return money";
+            echo "return money <br />";
         }else{
-            echo "More money";
+            echo "More money <br />";
         }
-
+    
     }
+
     public function returnMoney(){
         $returnCash = $this->cash;
         foreach($this->typeOfMoneyAndState as $type){
             //sprawdza ile bedzie do wydanai z określonego bilonu
-            $howMuchMoneyOfThisType = floor($returnCash / $type["value"]);
-            echo $returnCash / $type["value"] . " sd ";
+            $howMuchMoneyOfThisType = floor($returnCash / $type["value"] + 0.004);            
             if($howMuchMoneyOfThisType <= $type["quantity"]){
                 $returnCash = $returnCash - $howMuchMoneyOfThisType * $type["value"];
-                $type["quantity"] = $type["quantity"] - $howMuchMoneyOfThisType;
-                echo $howMuchMoneyOfThisType . " x " . $type["name"] . "  ";
+                $type["quantity"] = $type["quantity"] - $howMuchMoneyOfThisType;  
+                echo $howMuchMoneyOfThisType . " x " . $type["name"] . "  <br />";
             }elseif($type["quantity"] < $howMuchMoneyOfThisType && $type["quantity"] > 0 ){
                 //how much can you give away coins
                 $cacheCash = $howMuchMoneyOfThisType - $type["quantity"];
                 $returnCash = $returnCash - $cacheCash * $type["value"];
-                $type["quantity"] = $type["quantity"] - $cacheCash * $type["value"];
-                echo $cacheCash . " x " . $type["name"]  . "  ";
+                $type["quantity"] = $type["quantity"] - $cacheCash * $type["value"];  
+                echo $cacheCash . " x " . $type["name"]  . " <br /> ";
             }
         }
     }
-
-    public function addMoneyServis($i){
-        foreach($this->typeOfMoneyAndState as &$type){
-            if($type["id"] == $i){
-                $type["quantity"] = $type["quantity"] + 1;
-            }
-        }
-    }
-
-    public function deleteMoneyServis($i){
-        foreach($this->typeOfMoneyAndState as &$type){
-            if($type["id"] == $i){
-                $type["quantity"] = $type["quantity"] - 1;
-            }
-        }
-    }
-
-    public function addOrChangeItemServis($i, $j){
-        $this->items['item'.$i] = $j;
-    }
-
-    public function deleteItemServis($i){
-        unset($this->items["item".$i]);
-    }
-
 }
--- main.php
+++ main.php
@@ -2,17 +2,29 @@
 include("machine.php");
 class main{
     public function __construct(){
+        $this->start();
+    }
+    public function start(){
         $machine = new machine();
-        $machine->addMoney(0);
         // $machine->addMoney(1);
         // $machine->addMoney(1);
         // $machine->addMoney(1);
+        // $machine->addMoney(1);
+        // $machine->buyItem(1);
+        // $machine->returnMoney();
+
+        // $machine->addMoney(1);
+        // $machine->addMoney(1);
+        // $machine->returnMoney();
+
+
+        // $machine->addMoney(0);
+        // $machine->buyItem(0);
+        // $machine->returnMoney();
 
         // $machine->viewCash();
-       
-
-        $machine->deleteItemServis(1);
-        // $machine->buyItem(4);
+        // $machine->deleteItemServis(1);
+        // $machine->buyItem(0);
     }
 }
 $start = new main();
